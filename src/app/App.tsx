67ce82cfc1d3da7edc32e5302cd0824436cd41d3
Kerim-in <abu.hanifa.536942@gmail.com>
2023-03-04 14:25:28 +0300

    Sidebar Layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,7 @@ import {useTheme} from 'app/providers/ThemeProvider'
 import {classNames} from "shared/lib/classNames/classNames";
 import { AppRouter } from './providers/router';
 import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets";
 
 
 
@@ -14,7 +15,11 @@ const App = () => {
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar />
-            <AppRouter />
+            <div className="content-page">
+                <Sidebar />
+                <AppRouter />
+            </div>
+
         </div>
     );
 };
