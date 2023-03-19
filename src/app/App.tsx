308ca6c983de7196f298df6239edf5778c7f8164
Kerim-in <abu.hanifa.536942@gmail.com>
2023-03-20 02:20:51 +0300

    add modal, portal, change header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
-import React, { Suspense, useEffect } from 'react';
-import './styles/index.scss';
+import React, { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-// glob fix
+
 function App() {
     const { theme } = useTheme();
 
