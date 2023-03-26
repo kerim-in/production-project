9ae300f4b4427ff0058911385409d5fc9bab108d
Kerim-in <abu.hanifa.536942@gmail.com>
2023-03-26 07:28:22 +0300

    add new Input, add Login form ui

---
+++ src/entities/User/index.ts
@@ -0,0 +1,2 @@
+export { userReducer, userActions } from 'entities/User/model/slice/userSlice';
+export { UserSchema, User } from './model/types/user';
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
     const { t } = useTranslation();
+    const [value, setValue] = useState('');
 
+    const onChange = (val: string) => {
+        setValue(val);
+    };
     return (
         <div>
             {t('Главная страница')}
