1269f5d7f542e0caf0cc8dbae9a29c3f10bd0602
Kerim-in <abu.hanifa.536942@gmail.com>
2023-04-02 14:10:21 +0300

    add instance API, fix axios

--- src/features/AuthByUsername/model/selectors/getLoadingIsLoading/getLoadingIsLoading.ts
+++
@@ -1,3 +0,0 @@
-import { StateSchema } from 'app/providers/StoreProvider';
-
-export const getLoadingIsLoading = (state: StateSchema) => state.loginForm?.isLoading || false;
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,5 +1,6 @@
-import { classNames } from 'shared/lib/classNames/classNames';
+import { classNames, Mods } from 'shared/lib/classNames/classNames';
 import React, {
+    MutableRefObject,
     ReactNode, useCallback, useEffect, useRef, useState,
 } from 'react';
 import { Portal } from 'shared/ui/Portal/Portal';
@@ -27,7 +28,7 @@ export const Modal = (props: ModalProps) => {
 
     const [isClosing, setIsClosing] = useState(false);
     const [isMounted, setIsMounted] = useState(false);
-    const timerRef = useRef<ReturnType<typeof setTimeout>>();
+    const timerRef = useRef() as MutableRefObject<ReturnType<typeof setTimeout>>;
     const { theme } = useTheme();
 
     useEffect(() => {
@@ -68,7 +69,7 @@ export const Modal = (props: ModalProps) => {
         };
     }, [isOpen, onKeyDown]);
 
-    const mods: Record<string, boolean> = {
+    const mods: Mods = {
         [cls.opened]: isOpen,
         [cls.isClosing]: isClosing,
     };
@@ -80,9 +81,7 @@ export const Modal = (props: ModalProps) => {
     return (
         <Portal>
             <div className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}>
-                {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
                 <div className={cls.overlay} onClick={closeHandler}>
-                    {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
                     <div
                         className={cls.content}
                         onClick={onContentClick}
