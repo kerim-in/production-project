c4e062062777f7ef2ddf63cb22c3fb9121436126
Kerim-in <abu.hanifa.536942@gmail.com>
2023-04-08 02:05:30 +0300

    add articles page

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,7 +1,7 @@
 import React, { memo, Suspense, useCallback } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { PageLoader } from 'shared/ui/PageLoader/PageLoader';
-import routeConfig, { AppRoutesProps } from 'shared/config/routConfig/routeConfig';
+import { AppRoutesProps, routeConfig } from 'shared/config/routeConfig/routeConfig';
 import { RequireAuth } from 'app/providers/router/ui/RequireAuth';
 
 const AppRouter = () => {
---
+++ src/pages/ArticlesPage/index.ts
@@ -0,0 +1,3 @@
+export {
+    ArticlesPageAsync as ArticlePage,
+} from './ui/ArticlesPage/ArticlesPage.async';
