5ce6382d51492d1d638bbec945df6df0eafe3fd1
Kerim-in <abu.hanifa.536942@gmail.com>
2023-04-24 05:52:19 +0300

    add popover, add notifications

--- src/shared/ui/Popups/components/Dropdown/Dropdown.tsx
+++ src/shared/ui/Popups/components/Dropdown/Dropdown.tsx
@@ -4,6 +4,8 @@ import { Fragment, ReactNode } from 'react';
 import { DropdownDirection } from 'shared/types/ui';
 import { AppLink } from 'shared/ui/AppLink/AppLink';
 import cls from './Dropdown.module.scss';
+import { mapDirectionClass } from '../../styles/const';
+import popupCls from '../../styles/popup.module.scss';
 
 export interface DropdownItem {
     disabled?: boolean;
@@ -19,21 +21,14 @@ interface DropdownProps {
     trigger?: ReactNode;
 }
 
-const mapDirectionClass: Record<DropdownDirection, string> = {
-    'bottom left': cls.optionsBottomLeft,
-    'bottom right': cls.optionsBottomRight,
-    'top left': cls.optionsTopRight,
-    'top right': cls.optionsTopLeft,
-};
-
 export function Dropdown(props: DropdownProps) {
     const {
         className, items, trigger, direction = 'bottom right',
     } = props;
     const menuClasses = [mapDirectionClass[direction]];
     return (
-        <Menu as="div" className={classNames(cls.Dropdown, {}, [className])}>
-            <Menu.Button className={cls.btn}>
+        <Menu as="div" className={classNames(cls.Dropdown, {}, [className, popupCls.popup])}>
+            <Menu.Button className={popupCls.trigger}>
                 {trigger}
             </Menu.Button>
             <Menu.Items className={classNames(cls.menu, {}, menuClasses)}>
@@ -45,7 +40,7 @@ export function Dropdown(props: DropdownProps) {
                             onClick={item.onClick}
                             className={classNames(
                                 cls.item,
-                                { [cls.active]: active },
+                                { [popupCls.active]: active },
                             )}
                         >
                             {item.content}
