74971d1f0199eca73e7e628e532fe54745a3ee87
Kerim-in <abu.hanifa.536942@gmail.com>
2023-04-28 05:17:10 +0300

    fix unit with aliases, fix storybook after refactoring, update screens

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -11,11 +11,13 @@ export default {
         __API__: '',
         __PROJECT__: 'jest',
     },
+
     clearMocks: true,
     testEnvironment: 'jsdom',
     coveragePathIgnorePatterns: [
         '\\\\node_modules\\\\',
     ],
+
     moduleFileExtensions: [
         'js',
         'jsx',
@@ -24,22 +26,29 @@ export default {
         'json',
         'node',
     ],
+
     moduleDirectories: [
         'node_modules',
     ],
+
     modulePaths: [
         '<rootDir>src',
     ],
+
     testMatch: [
         // Обнаружил разницу между МАК ОС и ВИНДОУС!!!
         '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
     ],
+
     rootDir: '../../',
     setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+
     moduleNameMapper: {
         '\\.s?css$': 'identity-obj-proxy',
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+        '^@/(.*)$': '<rootDir>/src/$1',
     },
+
     reporters: [
         'default',
         ['jest-html-reporters', {
